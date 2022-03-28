6c85941bfe04e6237b6850b87c2ea0054b0a6cdc
Rastvor1tel <solvent71@yandex.ru>
2022-03-28 11:31:50 +0300

    Инит репозитория

---
+++ MegaplanAPI.php
@@ -0,0 +1,51 @@
+<?php
+
+class MegaplanAPI {
+	private string $token;
+
+	public function __construct(string $login = "", string $password = "", string $token = "") {
+		if ($token) {
+			$this->token = $token;
+		} else {
+			$this->auth($login, $password);
+		}
+	}
+
+	/**
+	 * Авторизация
+	 *
+	 * @param string $login Логин
+	 * @param string $password Пароль
+	 * @return void
+	 */
+	private function auth($login, $password): void {
+		$params = [
+			"username"   => $login,
+			"password"   => $password,
+			"grant_type" => "password"
+		];
+
+		$link = curl_init('https://gaps2.megaplan.ru/api/v3/auth/access_token');
+
+		curl_setopt($link, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($link, CURLOPT_POST, 1);
+		curl_setopt($link, CURLOPT_POSTFIELDS, http_build_query($params, '', '&'));
+
+		$output = curl_exec($link);
+
+		curl_close($link);
+
+		if ($output === false) {
+			echo 'cURL Error: ' . curl_error($link);
+			return;
+		}
+		$response = json_decode($output);
+
+		if (!$response->error) {
+			$result = $response->access_token;
+			$this->token = $result;
+			echo $result;
+		}
+	}
+
+}
